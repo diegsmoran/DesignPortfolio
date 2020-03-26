ec6b62e95388999bdc97b6972db4526da6da6936
diegsmoran <diegomoran18@gmail.com>
2020-03-26 14:59:56 -0700

    EDIT

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Define the modals
+
+var modal = document.getElementById("myModal");
+
+// When the user clicks anywhere outside of the modal, close it
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
